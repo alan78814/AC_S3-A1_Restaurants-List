2d7eeae916fa5245201ce171279d885ff9558811
alan78814 <tri78814@gmail.com>
2021-07-13 12:08:00 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -8,44 +8,25 @@ const restaurantList = require('./restaurant.json')
 // 建立  model /require('./models/restaurant.js')  = mongoose.model('Restaurant', restaurantSchema)
 const Restaurant = require('./models/restaurant.js')
 // 載入 method-override
-const methodOverride = require('method-override') 
+const methodOverride = require('method-override')
+// 引用路由器
+const routes = require('./routes')
 
-//載入mongoose
-const mongoose = require('mongoose')
-// 設定連線到 mongoDB 連線到restaurant-list資料夾
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true }) 
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
+// 載入 mongoose.js
+require('./config/mongoose')
 // setting template engine 固定使用語法格式
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(express.urlencoded({ extended: true })) //改寫成 express
-
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
-
-// 引用路由器
-const routes = require('./routes')
 // 將 request 導入路由器
 app.use(routes)
-
-
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
-
 // setting static files
 app.use(express.static('public'))
 
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -2,16 +2,7 @@ const Restaurant = require('../restaurant') // 載入 restaurant.js
 const rawData = require('../../restaurant.json')
 const seedData = rawData.results
 
-//載入mongoose
-const mongoose = require('mongoose')
-// 設定連線到 mongoDB 連線到restaurant-list資料夾
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true }) 
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
 // 連線成功
 db.once('open', () => {
     seedData.forEach((restaurant) => {
@@ -28,5 +19,5 @@ db.once('open', () => {
             description: restaurant.description
         })
     })
-    console.log('mongodb connected!')
+    console.log('restaurant seeder done')
 })
