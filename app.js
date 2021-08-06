33aad211fcc86452dafd93f165b7ddcf3b7c90e5
alan78814 <tri78814@gmail.com>
2021-08-06 14:57:03 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,56 +1,54 @@
-// require packages used in the project
 const express = require('express')
 const app = express()
 const port = 3000
-// require express-handlebars here 固定使用語法格式
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
-// 建立  model /require('./models/restaurant.js')  = mongoose.model('Restaurant', restaurantSchema)
 const Restaurant = require('./models/restaurant.js')
-// 載入 method-override
 const methodOverride = require('method-override')
-// 引用路由器
 const routes = require('./routes')
-// 載入 mongoose.js
+const session = require('express-session')
+
 require('./config/mongoose')
-// setting template engine 固定使用語法格式
+
 app.engine('handlebars', exphbs({
   defaultLayout: 'main',
   // 保留選擇資料
   helpers: {
     checkAsc: function (name) {
       if (name === "asc")
-      return "selected"
+        return "selected"
     },
     checkDesc: function (name) {
-      if(name === "desc")
-      return "selected"
+      if (name === "desc")
+        return "selected"
     },
     checkCategory: function (name) {
-      if(name === "category")
-      return "selected"
+      if (name === "category")
+        return "selected"
     },
     checkLocation: function (name) {
-      if(name === "location")
-      return "selected"
+      if (name === "location")
+        return "selected"
     },
     checkRating: function (name) {
-      if(name === "rating")
-      return "selected"
+      if (name === "rating")
+        return "selected"
     },
   }
 }))
 app.set('view engine', 'handlebars')
-// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(express.urlencoded({ extended: true })) //改寫成 express
-// 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
-// 將 request 導入路由器
+app.use(express.static('public'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(routes)
-// start and listen on the Express server
+
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
-// setting static files
-app.use(express.static('public'))
+
 
