5bef5c79655af8ed8d28711bc3362c4c73cc2c59
alan78814 <tri78814@gmail.com>
2021-08-06 10:45:41 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,15 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
-
-// 引入 home 模組程式碼
 const home = require('./modules/home')
-// 將網址結構符合 / 字串的 request 導向 home 模組 
+const restaurants = require('./modules/restaurants')
+const search = require('./modules/search')
+const users = require('./modules/users')
+
+
+router.use('/restaurants', restaurants)
+router.use('/search', search)
+router.use('/users', users)
 router.use('/', home)
 
-// 引入 restaurants 模組程式碼
-const restaurants = require('./modules/restaurants')
-// 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
-router.use('/restaurants', restaurants)
-
-// 引入 search 模組程式碼
-const search = require('./modules/search')
-// 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
-router.use('/search', search)
-
-// 匯出路由器
 module.exports = router
